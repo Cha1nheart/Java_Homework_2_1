51c89c97b1b278d417646f847feda38301a1cb47
Cha1nHeart <humanoverrated@gmail.com>
2023-03-12 11:19:27 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13_676;
+        int Miles = 20;
+
+        int bonusMiles = (ticket / Miles);
+
+        System.out.println("Начислено бонусных миль: +" + bonusMiles);
+
+    }
+}
